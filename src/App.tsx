d6d22bf47ba233f4adf2ee7f8d3b99c6e3ac329e
SakaueYae <sky.bgm.tdrksht.132133@gmail.com>
2024-11-04 03:25:05 +0900

    supabaseの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import { Button } from "./components/ui/button";
+import { supabase } from "./utils/supabase";
 
 function App() {
+  const getData = async () => {
+    try {
+      const res = await supabase.from("users").select("*");
+      console.log(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  getData();
+
   return (
     <div>
       <Button>Click me</Button>
